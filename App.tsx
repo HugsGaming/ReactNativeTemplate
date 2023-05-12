c88e28ae567a67b44170310ae6bd5c82ff6e248c
HugsGaming <jovenreyes020803@gmail.com>
2023-05-12 21:13:47 +0800

    Initialize Project

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+
+
+function App(): JSX.Element {
+  //const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  );
+}
+
+export default App;
